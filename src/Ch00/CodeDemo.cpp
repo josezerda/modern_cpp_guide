48129b55d62fb1e32f7062fcb19f5826be3ebfe7
Jose Zerda <jozerda@gmail.com>
2026-01-03 23:34:40 +0000

    First commit

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -7,9 +7,9 @@
 int main(){
     float num_1, num_2, result;
 
-    std::cout << "Enter number 1: " << std::flush;
+    std::cout << "Enter number A: " << std::flush;
     std::cin >> num_1;
-    std::cout << "Enter number 2: " << std::flush;
+    std::cout << "Enter number B: " << std::flush;
     std::cin >> num_2;
     
     result = num_1 + num_2;
